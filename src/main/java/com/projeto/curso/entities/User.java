8a8c42e4d1d499d7711ca6f381fc00e698a04dbc
Kauan Ferreira <kfr@academico.ufpb.br>
2024-02-15 15:43:45 -0300

    H2 e JPA

--- src/main/java/com/projeto/curso/entities/User.java
+++ src/main/java/com/projeto/curso/entities/User.java
@@ -2,7 +2,17 @@ package com.projeto.curso.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
@@ -17,36 +27,45 @@ public class User implements Serializable{
         this.password = password;
     }
 
-    public User(){
+    public User() {
     }
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
